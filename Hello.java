60f80f614213ee382d6549dc8166571d32eb8fd6
Raturi7 <79138283+raturi7@users.noreply.github.com>
2022-05-25 00:14:03 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  Public static void main(String[] args) {
+  public static void main(String[] args) {
     // Prints "Hello, World" in the terminal window.
     System.out.println("Hello, World");
   }
